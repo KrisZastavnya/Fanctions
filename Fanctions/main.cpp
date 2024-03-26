cc64e9aa389c901d8de9817a0eeae75f9d3f101c
Kris <bysinkakristl@gmail.com>
2024-03-26 14:01:27 +0300

    Add project files.

---
+++ Fanctions/main.cpp
@@ -0,0 +1,43 @@
+//Fanctions
+#include<iostream>
+using namespace std;
+
+int Sum(int a, int b);
+int Diff(int a, int b);
+int Prod(int a, int b);
+double Quote(int a, int b);
+
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a, b;
+	cout << "¬ведите два числа: "; cin >> a >> b;
+	int c = Sum(a, b);
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Diff(a, b) << endl;
+	cout << a << " * " << b << " = " << Prod(a, b) << endl;
+	cout << a << " / " << b << " = " << Quote(a, b) << endl;
+}
+
+	int Sum(int a, int b)
+	{
+		int c = a + b;
+		return c;
+	}
+	int Diff(int a, int b)
+	{
+		//Deff(difference) - разность 
+		return a - b;
+	}
+	int Prod(int a, int b)
+	{
+		//Prod(product) - произведение
+		return a * b;
+	}
+	double Quote(int a, int b)
+	{
+		//Quote(Quotient) - частное
+		return (double)a / b;
+	}
